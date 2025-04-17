dcf9855851d89eec275b57b9e72605995501f737
Carlos Valderrama <cvalderrama1991@gmail.com>
2025-04-16 22:22:21 -0400

    deleted the scrips directory and added a utilities directory to store resuable function

--- public/app.js
+++ public/app.js
@@ -1,3 +1,9 @@
+import { removeDuplicates } from '../utils/index.js';
+
 const main = document.querySelector('main');
 
-main.innerText = 'Main Content';
+const numbers = [1, 2, 3, 4, 5, 5, 3, 2, 1];
+
+console.log(removeDuplicates(numbers));
+console.log(numbers);
+
---
+++ utils/index.js
@@ -0,0 +1,11 @@
+export function removeDuplicates(arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
