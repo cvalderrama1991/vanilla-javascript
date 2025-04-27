b033516d755654ef3ee8170047d383dbd0055bda
Carlos Valderrama <cvalderrama1991@gmail.com>
2025-04-27 14:29:28 -0400

    styled the header, nav, and theme options

--- public/app.js
+++ public/app.js
@@ -1,18 +1,1 @@
-import { removeDuplicates } from '../utils/index.js';
-
-const main = document.querySelector('main');
-
-const numbers = [1, 2, 3, 4, 5, 5, 3, 2, 1];
-
-console.log(removeDuplicates(numbers));
-console.log(numbers);
-
-function add(nms) {
-let total = 0;
-for (let i = 0; i < nms.length; i++) {
-	total = total + nms[i];
-	}
-	return total;
-}
-
-console.log([1,2,3]);
+console.log('Hello World!');
