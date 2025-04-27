60a46fed4cb70fb8f7c95dfa10a15260b1badd62
Carlos Valderrama <cvalderrama1991@gmail.com>
2025-04-27 14:10:00 -0400

    removed .env file

--- public/app.js
+++ public/app.js
@@ -7,3 +7,12 @@ const numbers = [1, 2, 3, 4, 5, 5, 3, 2, 1];
 console.log(removeDuplicates(numbers));
 console.log(numbers);
 
+function add(nms) {
+let total = 0;
+for (let i = 0; i < nms.length; i++) {
+	total = total + nms[i];
+	}
+	return total;
+}
+
+console.log([1,2,3]);
