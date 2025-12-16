c43c3e00d42c4f9d1f46f6f86315b00de9cb823b
Carlos Valderrama <cvalderrama1991@gmail.com>
2025-12-15 22:35:30 -0500

    native css and manual theme setup

--- public/app.js
+++ public/app.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+const root = document.querySelector("[data-theme]");
+const theme_selector = document.querySelector("[data-selector=theme]");
+const STORAGE_KEY = 'theme';
+
+// Load Saved Theme
+function loadTheme() {
+  const savedTheme = localStorage.getItem(STORAGE_KEY) || 'systeme';
+
+  root.setAttribute("data-theme", savedTheme);
+  theme_selector.value = savedTheme;
+}
+
+// Save and Apply Theme
+function setTheme(theme) {
+  root.setAttribute("data-theme", theme);
+  localStorage.setItem(STORAGE_KEY, theme);
+}
+
+theme_selector.addEventListener('change', (e) => {
+  setTheme(e.target.value);
+
+  console.log(theme_selector.value);
+});
+
+// Initial Theme
+loadTheme();
+
